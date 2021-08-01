3ec790abee30b3c132e88a1df2e7d32fc4c9ccc6
IRISLI0224 <38321032+IRISLI0224@users.noreply.github.com>
2021-08-01 19:34:25 +0800

    add addsession page

--- src/routes/allRoutes.js
+++ src/routes/allRoutes.js
@@ -5,6 +5,7 @@ import Home from '../pages/Home';
 import Login from '../pages/Login';
 import MyBooking from '../pages/MyBooking';
 import AdminBookingDetail from '../pages/AdminBookingDetail';
+import AddSession from '../pages/Admin/AddSession';
 
 // 不需要登录即可访问的路由
 export const commonRoutes = [
@@ -21,4 +22,6 @@ export const authRoutes = [
   // { path: '/admin', component: Admin },
   { path: '/admin/guestlist', component: GuestList },
   { path: '/admin/bookingdetail', component: AdminBookingDetail },
+  { path: '/admin/addsession', component: AddSession },
+
 ];
