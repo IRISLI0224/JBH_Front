f10552db91d10b1ef1772f9be591887bf2a5e1fb
chen hong <vvchennnnn@gmail.com>
2021-08-06 19:37:04 +1000

    for update

--- src/pages/Admin/AddSession/components/Calendar/components/CalendarDay/setDayStyles.js
+++ src/pages/Admin/AddSession/components/Calendar/components/CalendarDay/setDayStyles.js
@@ -5,8 +5,8 @@ const colors = {
   fullyBooked: '#77AFCF',
 };
 
-const notCurrMonth = (day, value) => day.isBefore(value.clone().startOf('month'), 'day')
-  || day.isAfter(value.clone().endOf('month'), 'day');
+const notCurrMonth = (day, value) =>
+  day.isBefore(value.clone().startOf('month'), 'day') || day.isAfter(value.clone().endOf('month'), 'day');
 const isToday = (day) => day.isSame(new Date(), 'day');
 const beforeToday = (day) => day.isBefore(new Date(), 'day');
 
@@ -15,10 +15,10 @@ const dayStyles = (day, value, monthlySessions) => {
   const bgColor = notCurrMonth(day, value)
     ? '#fff'
     : beforeToday(day)
-      ? '#F0F8FF'
-      : colors[monthlySessions[day.format('D') * 1 - 1]];
-  const numColor = notCurrMonth(day, value) ? '#fff' : isToday(day) ? '#7A6263' : '#000';//eslint-disable-line
-
+    ? '#F0F8FF'
+    : colors[monthlySessions[day.format('D') * 1 - 1]];
+  const numColor = notCurrMonth(day, value) || isToday(day) ? '#181b50' : '#000';
+  // 根据三个日期判断条件和session状态array，返回背景色和日期色供日历使用
   return { bgColor, numColor };
 };
 
--- src/pages/Admin/components/Calendar/components/CalendarDay/setDayStyles.js
+++ src/pages/Admin/components/Calendar/components/CalendarDay/setDayStyles.js
@@ -3,15 +3,12 @@ const colors = {
   true: '#6495ED',
 };
 
-const notCurrMonth = (day, value) => day.isBefore(value.clone().startOf('month'), 'day')
-  || day.isAfter(value.clone().endOf('month'), 'day');
+const notCurrMonth = (day, value) =>
+  day.isBefore(value.clone().startOf('month'), 'day') || day.isAfter(value.clone().endOf('month'), 'day');
 
 const dayStyles = (day, value, monthlySessions) => {
-  const bgColor = notCurrMonth(day, value)
-    ? '#fff'
-    : colors[monthlySessions[day.format('D') * 1 - 1]];
+  const bgColor = notCurrMonth(day, value) ? '#fff' : colors[monthlySessions[day.format('D') * 1 - 1]];
   const numColor = notCurrMonth(day, value) ? '#fff' : '#000';
-
   return { bgColor, numColor };
 };
 
