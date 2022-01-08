41f4f01b8ebc4f16a3b345b600301d02799e5ecf
Kazoooo <likefu308@gmail.com>
2022-01-08 11:35:55 +1100

    change backend server

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -5,7 +5,7 @@ module.exports = {
   devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.BASE_URL': JSON.stringify('http://3.25.70.196:3000'),
+      'process.env.BASE_URL': JSON.stringify('http://3.106.127.147:3000'),
     }),
   ],
 };
